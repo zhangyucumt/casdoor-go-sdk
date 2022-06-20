a2ab12a4b67d4becfff344632dcbff23a457f079
刘宪东 <liuxiandong10@gmail.com>
2022-06-20 15:45:43 +0800

    Update setting.go

--- auth/setting.go
+++ auth/setting.go
@@ -3,8 +3,8 @@ package auth
 type Setting struct {
 	*CommonObject
 
-	Value       string `xorm:"varchar(255) notnull" json:"value"`
-	Description string `xorm:"varchar(255) notnull" json:"description"`
+	Value       interface{} `xorm:"notnull" json:"value"`
+	Description string      `xorm:"varchar(255) notnull" json:"description"`
 
 	CreatedTime string `xorm:"varchar(100) created_time created" json:"createdTime"`
 }
